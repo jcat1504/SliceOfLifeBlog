6ef22180cf8acab5266bd37c69f59a9ce2504775
Jess Y <jyin978@hotmail.com>
2023-11-10 19:42:23 -0500

    Home component

--- slice-of-life-react/src/App.js
+++ slice-of-life-react/src/App.js
@@ -1,41 +1,16 @@
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-
-function Home() {
-  return <h2>Welcome to the Home Page</h2>;
-}
-
-function About() {
-  return <h2> Learn more about me!</h2>;
-}
-
-function Contact() {
-  return <h2>Contact me here</h2>;
-}
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/"> Home </Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact"> Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path ="/contact" component={Contact} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
       </div>
-    </Router>
+    </div>
   );
 }
 
 export default App;
+
---
+++ slice-of-life-react/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    const searchQuery = useLocation().search;
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`posts${searchQuery}`);
+                setPosts(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [searchQuery]);
+
+    const getText = html => {
+        const doc = new DOMParser().parseFromString(html, "text/html");
+        return doc.body.textContent;
+    };
+
+    return (
+        <div className="home">
+            <div className="posts">
+                {posts.map((post) => {
+                    <div className="post" key={post.id}>
+                        <div className="post-img">
+                            <img src={`.../upload/${post.img}`} alt="post img cover" />
+                        </div>
+                        <div className="content">
+                            <Link className="link" to={`/post/${post.id}`}>
+                            <h1>{post.title}</h1>
+                            </Link>
+                            <p>{getText(post.desc)}</p>
+                            <Link className="link" to={`/post/${post.id}`}>
+                                <button>Read More</button>
+                            </Link>
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
