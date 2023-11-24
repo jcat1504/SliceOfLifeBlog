2f185c6cf953c3096ed4f238699b67335417cac4
jcat1504 <jyin978@hotmail.com>
2023-11-24 14:44:05 -0500

    footer component

--- slice-of-life-react/src/components/Footer.jsx
+++ slice-of-life-react/src/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
-    return <div> Footer</div>
+    return (
+        <footer>
+            <span>
+                <br />
+                Made with 🤍 by:
+                <br />
+                <a href="https://github.com/jcat1504"><b>@Jess Y.</b></a>
+            </span>
+        </footer>
+    )
 };
 
 export default Footer;
