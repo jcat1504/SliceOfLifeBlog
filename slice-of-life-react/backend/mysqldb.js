a64d32fc5bbf30c7fa6b1d7092713b3ba8ff1333
Jess Y <jyin978@gmail.com>
2023-12-07 17:29:33 -0500

    db code local connection

--- slice-of-life-react/backend/mysqldb.js
+++ slice-of-life-react/backend/mysqldb.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: null,
-    database: "sliceoflife",
+    host: rocess.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
